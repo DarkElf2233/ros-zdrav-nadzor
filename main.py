698a6a5f3a25ec28211ab95d1161985e60a55ff1
DarkElf2233 <fd.melnikoff@gmail.com>
2024-08-13 17:25:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+import sys
 import time
 
-# options = webdriver.ChromeOptions()
-# options.add_argument('--headless')
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
 
-driver = webdriver.Chrome()  # options=options
+driver = webdriver.Chrome(options=options)
 driver.get("https://roszdravnadzor.gov.ru/services/mi_reesetr")
 
 try:
-    tr = driver.find_element(By.CLASS_NAME, "asfsfsfs")
+    tr = driver.find_element(By.CLASS_NAME, "odd")
 except:
-    print('Error')
     driver.close()
+    sys.exit()
 
 btn = driver.find_element(By.CLASS_NAME, "ico-xls")
+btn.click()
+
+time.sleep(3 * 60)
 
 driver.close()
